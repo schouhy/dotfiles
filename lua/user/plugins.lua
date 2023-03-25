e5187daffbbe4a46162587a47767e357b08b7edc
Sergio Chouhy <sergio.chouhy@gmail.com>
2023-03-25 18:53:24 -0300

    add vim-visual-multi

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -111,6 +111,9 @@ return packer.startup(function(use)
   -- Rust
   use { "simrat39/rust-tools.nvim" }
 
+  -- Multi Cursor
+  use { "mg979/vim-visual-multi"}
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
