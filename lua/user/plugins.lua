44138e41da6f3d92e71ca5772841b9cd7816f05e
ajgarassino <ajgarassino@gmail.com>
2023-03-27 15:57:15 -0300

    Add autosave

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -114,6 +114,9 @@ return packer.startup(function(use)
   -- Multi Cursor
   use { "mg979/vim-visual-multi"}
 
+  -- Autosave
+  use { "Pocco81/auto-save.nvim" }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
