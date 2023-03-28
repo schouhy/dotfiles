58d9914557df405259934e041a9acc9f75283368
Sergio Chouhy <sergio.chouhy@gmail.com>
2023-03-28 08:11:30 -0300

    change diagnostics icons

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -7,12 +7,10 @@ local actions = require "telescope.actions"
 
 telescope.setup {
   defaults = {
-
     prompt_prefix = " ",
     selection_caret = " ",
     path_display = { "smart" },
     file_ignore_patterns = { ".git/", "node_modules" },
-
     mappings = {
       i = {
         ["<Down>"] = actions.cycle_history_next,
