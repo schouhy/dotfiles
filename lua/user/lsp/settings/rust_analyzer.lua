c9d70e35738875e2004e32cce2c2875cde8b0b6e
Sergio Chouhy <sergio.chouhy@gmail.com>
2023-04-01 16:32:34 -0300

    mason codelldb

--- lua/user/lsp/settings/rust_analyzer.lua
+++ lua/user/lsp/settings/rust_analyzer.lua
@@ -163,6 +163,19 @@ M.opts = {
     -- setting it to false may improve startup time
     standalone = true,
   }, -- rust-analyzer options
+    -- debugging stuff
+  dap = {
+    adapter = {
+    type = "server",
+    host = "127.0.0.1",
+    port = "${port}",
+    executable ={
+      command = vim.fn.stdpath("data") .. "/mason/bin/codelldb",
+      args = { "--port", "${port}" }
+    },
+    name = "codelldb",
+    },
+  },
 }
 
 function M.setup(options)
