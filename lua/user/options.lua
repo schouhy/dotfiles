b236531763cabedd7ded90276d212b39c3c86ada
Sergio Chouhy <sergio.chouhy@gmail.com>
2023-08-25 20:02:04 -0300

    update to nvim 0.10

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -41,3 +41,4 @@ vim.opt.iskeyword:append("-")                   -- treats words with `-` as sing
 vim.opt.formatoptions:remove({ "c", "r", "o" }) -- This is a sequence of letters which describes how automatic formatting is to be done
 vim.opt.linebreak = true
 vim.opt.relativenumber = true
+vim.opt.so = 50
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -49,7 +49,7 @@ return packer.startup(function(use)
   use { "numToStr/Comment.nvim"}
   use { "JoosepAlviste/nvim-ts-context-commentstring"}
   use { "kyazdani42/nvim-web-devicons"}
-  use { "kyazdani42/nvim-tree.lua"}
+  use { "nvim-tree/nvim-tree.lua"}
   use { "akinsho/bufferline.nvim"}
   use { "moll/vim-bbye"}
   use { "nvim-lualine/lualine.nvim"}
